c5a6b9818f91f3f1dd344be9ec1a13de69cab999
GODOL <sihyun9346@mokwon.ac.kr>
2025-10-27 15:07:51 +0900

    Add BulletFire & Change Bullet

--- MusicLand/Assets/Scripts/PlayerControls.cs
+++ MusicLand/Assets/Scripts/PlayerControls.cs
@@ -3,25 +3,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    #region 설정
-    private PlayerMove controls;    // InputActions 클래스
+    #region 변수선언
+    private PlayerMove controls;
     private Vector2 moveInput;
-    private Animator playerAnimator;    // 애니메이터 클래스 설정
+    private Animator playerAnimator;
     private SpriteRenderer playerSpriteRenderer;
     private Rigidbody2D rb;
+    #endregion
+
+    // 캐릭터 움직임(속도, 점프) 세팅
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
-    #endregion
 
+    // 캐릭터 공격지점, BulletManager 불러오기
+    [Header("Attack Settings")]
+    public Transform firePoint;
+    public BulletManager bulletManager;
+    // FirePoint의 초기 로컬 위치 저장
+    private Vector3 firePointOriginalLocalPos;
+
+    // 캐릭터 기본 바닥여부 -> True
     private bool isGrounded = true;
 
+
+    // 컴포넌트 가져오기
     private void Awake()
     {
         controls = new PlayerMove();
         rb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
 
+        // FirePoint 원래 위치 저장
+        if (firePoint != null)
+            firePointOriginalLocalPos = firePoint.localPosition;
+
+        #region NewInputSystem 세팅
         // 이동 입력 처리
         controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         controls.Player.Move.canceled += _ => moveInput = Vector2.zero;
@@ -29,63 +47,75 @@ public class PlayerControls : MonoBehaviour
         // 점프 입력 처리
         controls.Player.Jump.performed += _ => Jump();
 
-        // SpriteRenderer 가져오기
-        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        // 공격 입력 처리
+        controls.Player.Attack.performed += _ => Attack();
     }
 
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-    }
-
-    private void OnDisable()
-    {
-        controls.Player.Disable();
-    }
+    // Input System 활성화
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
+    #endregion
 
     private void Update()
     {
         Move();
         UpdateAnimation();
+        UpdateFirePointDirection(); // FirePoint 좌우 반전 처리
     }
 
     private void Move()
     {
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
 
-        // 좌우반전
+        // 좌우 반전
         if (moveInput.x > 0)
-        {
-            playerSpriteRenderer.flipX = false; // 오른쪽 이동 → 기본 방향
-        }
+            playerSpriteRenderer.flipX = false;
         else if (moveInput.x < 0)
-        {
-            playerSpriteRenderer.flipX = true;  // 왼쪽 이동 → 반전
-        }
+            playerSpriteRenderer.flipX = true;
     }
 
     private void Jump()
     {
+        // isGround일 떄만 점프가 가능하도록 설정
         if (!isGrounded) return;
-        {
-
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-        }
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isGrounded = false;
     }
-    // 땅과 충돌 시 점프 기능 상태로 복구
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // 땅 충돌판정시 isGrounded false -> true 변경
         if (collision.contacts.Length > 0 && collision.contacts[0].normal.y > 0.3f)
-        {
             isGrounded = true;
-        }
     }
 
     private void UpdateAnimation()
     {
+        // 캐릭터 움직이면 Run 애니메이션 재생
         bool isRunning = Mathf.Abs(moveInput.x) > 0.1f;
-
         playerAnimator.SetBool("isRunning", isRunning);
+    }
+
+    private void UpdateFirePointDirection() // 캐릭터 좌우반전 FirePoint에도 적용
+    {
+        if (firePoint == null) return;
+
+        Vector3 localPos = firePointOriginalLocalPos;
+        if (playerSpriteRenderer.flipX)
+            localPos.x = -Mathf.Abs(localPos.x);
+        else
+            localPos.x = Mathf.Abs(localPos.x);
+
+        firePoint.localPosition = localPos;
+    }
+
+    private void Attack() // FirePoint 위치에서 총알 발사
+    {
+        if (bulletManager == null || firePoint == null) return;
+
+        Vector2 fireDir = playerSpriteRenderer.flipX ? Vector2.left : Vector2.right;
+
+        // FireBullet 호출 시 BulletManager의 현재 타입 사용
+        bulletManager.FireBullet(firePoint.position, fireDir);
     }
 }
--- MusicLand/Assets/Scripts/PlayerMove.cs
+++ MusicLand/Assets/Scripts/PlayerMove.cs
@@ -109,6 +109,24 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""1b9d9fc3-1472-4342-acdf-00f53cb28974"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Paring"",
+                    ""type"": ""Button"",
+                    ""id"": ""3dc1b662-62df-4217-a02d-db8c27805c4d"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -236,11 +254,33 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""6848a87d-bf45-4f39-aa3a-bb54a6b16cbe"",
-                    ""path"": ""<Keyboard>/z"",
+                    ""path"": ""<Keyboard>/alt"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0690951f-6fcf-4563-ace5-48d6d6756771"",
+                    ""path"": ""<Keyboard>/ctrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";PC"",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""aadfa836-b5db-4af3-9338-3055aab3c996"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Paring"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -270,6 +310,8 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
+        m_Player_Paring = m_Player.FindAction("Paring", throwIfNotFound: true);
     }
 
     ~@PlayerMove()
@@ -352,6 +394,8 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Attack;
+    private readonly InputAction m_Player_Paring;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -371,6 +415,14 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Jump".
         /// </summary>
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Attack".
+        /// </summary>
+        public InputAction @Attack => m_Wrapper.m_Player_Attack;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Paring".
+        /// </summary>
+        public InputAction @Paring => m_Wrapper.m_Player_Paring;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -403,6 +455,12 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
             @Jump.started += instance.OnJump;
             @Jump.performed += instance.OnJump;
             @Jump.canceled += instance.OnJump;
+            @Attack.started += instance.OnAttack;
+            @Attack.performed += instance.OnAttack;
+            @Attack.canceled += instance.OnAttack;
+            @Paring.started += instance.OnParing;
+            @Paring.performed += instance.OnParing;
+            @Paring.canceled += instance.OnParing;
         }
 
         /// <summary>
@@ -420,6 +478,12 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
             @Jump.started -= instance.OnJump;
             @Jump.performed -= instance.OnJump;
             @Jump.canceled -= instance.OnJump;
+            @Attack.started -= instance.OnAttack;
+            @Attack.performed -= instance.OnAttack;
+            @Attack.canceled -= instance.OnAttack;
+            @Paring.started -= instance.OnParing;
+            @Paring.performed -= instance.OnParing;
+            @Paring.canceled -= instance.OnParing;
         }
 
         /// <summary>
@@ -487,5 +551,19 @@ public partial class @PlayerMove: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnJump(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Attack" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnAttack(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Paring" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnParing(InputAction.CallbackContext context);
     }
 }
