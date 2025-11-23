7b5b0b860f446c63f435f8f32d283c887fefee97
GODOL <sihyun9346@mokwon.ac.kr>
2025-11-23 18:16:05 +0900

    Import Character Animation
    캐릭터 애니메이션 추가(idle, run, jump)

--- MusicLand/Assets/Scripts/PlayerManager.cs
+++ MusicLand/Assets/Scripts/PlayerManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    #region 캐릭터 움직임 정리
+    #region inspector 정리
 
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
 
     [Header("Player Settings")]
     [SerializeField] float speed;
@@ -17,25 +18,42 @@ public class PlayerManager : MonoBehaviour
     [Header("Grounding")]
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
+    #endregion
 
     private float horizontal;
 
     private void FixedUpdate()
     {
+        // 이동 처리
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // Animator 관리
+        animator.SetBool("isRunning", Mathf.Abs(horizontal) > 0.05f);
+
+        // isGrounded 체크
+        bool grounded = IsGrounded();
+        animator.SetBool("isGrounded", grounded);
+
+        // 착지시 점프 종료
+        if (grounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
+    #region 캐릭터 움직임 관련 정리
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
     }
-    #endregion
+
 
     public void Jump(InputAction.CallbackContext context)
     {
         if(context.performed && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            animator.SetBool("isJumping", true);
         }
     }
 
@@ -43,5 +61,11 @@ public class PlayerManager : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.7f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+    #endregion
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        
     }
 }
